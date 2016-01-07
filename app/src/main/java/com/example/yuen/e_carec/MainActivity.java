66d92fc83be0de155208bdd4d8086c65781d72d7
ttchiuaa <daviddach917@gmail.com>
2016-01-07 13:23:45 +0800

    safsadas

--- app/src/main/java/com/example/yuen/e_carec/MainActivity.java
+++ app/src/main/java/com/example/yuen/e_carec/MainActivity.java
@@ -15,7 +15,7 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
+asdas
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
